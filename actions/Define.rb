ee80d1dbf81a70bee6dc680b8bbe6d7f88fd9a7f
Gordon Fontenot <gordon.fontenot@gmail.com>
2011-02-08 13:56:57 -0500

    fixed define. Fixed help if no desc is given

--- actions/Define.rb
+++ actions/Define.rb
@@ -11,6 +11,15 @@ class Define
     /define (.*)/i
   end
   
+  def desc_short
+    "define WORD"
+  end
+  
+  def desc_long
+    "Use Google too look up the definition for the given word"
+  end
+  
+  
   def perform matchdata
     define_word(matchdata[1])
   end
@@ -20,7 +29,7 @@ class Define
     page = Nokogiri::HTML(open("http://www.google.com/search?hl=en&q=define:#{URI.encode(word)}"))
     definition = page.css("ul.std li").first
 
-    @room.speak "None found" if definition.nil?
+    @room.speak "Can't find the definition of #{word}. Are you sure that it's a word?" if definition.nil?
 
     @room.speak definition.inner_html.gsub('<br>', ' - ').gsub(%r{</?[^>]+?>}, '')
   end
